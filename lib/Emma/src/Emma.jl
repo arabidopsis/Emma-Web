4d3522a95263a33594709dad6df51c0b6a9a7225
arabidopsis <ian.castleden@gmail.com>
2024-08-11 03:24:44 +0800

    moved

--- lib/Emma/src/Emma.jl
+++ lib/Emma/src/Emma.jl
@@ -9,15 +9,15 @@ using Logging
 using Unicode
 using GenomicAnnotations
 using UUIDs
-using Pkg.Artifacts
+# using Pkg.Artifacts
 
 export main, doone, writeGFF, tempfilename, TempFile, drawgenome, trnF_start
 
 
-const DATA = joinpath(@__DIR__, "..")
-const emmamodels = joinpath(DATA, "emma_vertebrate_models")
+const DATA = joinpath(@__DIR__, "..", "data")
+# const emmamodels = joinpath(@__DIR__, "..", "emma_vertebrate_models")
 # const emmamodels = joinpath(artifact"Emma-models","emma-models-0.1.5-alpha")
-# const emmamodels = joinpath(artifact"Emma2-models","emma-models-1.0.0")
+const emmamodels = joinpath(artifact"Emma2-models","emma-models-1.0.0")
 include("tempfile.jl")
 include("circularity.jl")
 include("feature.jl")
